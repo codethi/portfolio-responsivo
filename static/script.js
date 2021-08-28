7beebf8614b7c8803c6c037a3db4316e6d86d51e
codethi <docencia.thiago@gmail.com>
2021-08-28 10:39:08 -0300

    seção sobre e habilidades

--- static/script.js
+++ static/script.js
@@ -9,3 +9,17 @@ menuMobile.addEventListener("click", () => {
     : menuMobile.classList.replace("bi-x", "bi-list");
   body.classList.toggle("menu-nav-active");
 });
+
+
+/* Fecha o menu quando clicar em algum item e muda o icone para list */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active")
+      menuMobile.classList.replace("bi-x", "bi-list");
+    }
+  })
+})
